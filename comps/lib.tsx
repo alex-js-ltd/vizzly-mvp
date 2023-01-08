b81a4d8b47c1c61277476732742b23db55850a1e
Alex Gilbert <alex.js.ltd@gmail.com>
2023-01-08 13:51:01 +0000

    update style on select

--- comps/lib.tsx
+++ comps/lib.tsx
@@ -90,6 +90,7 @@ const FormGroup = styled.div({
   marginTop: 20,
   display: 'flex',
   justifyContent: 'space-between',
+  width: '100%',
 });
 
 const Input = styled.input({ borderRadius: '3px' }, inputStyles);
@@ -97,7 +98,7 @@ const Input = styled.input({ borderRadius: '3px' }, inputStyles);
 const icon = `"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' opacity='0.5' aria-hidden='true' height='16' viewBox='0 0 16 16' version='1.1' width='16' data-view-component='true' class='octicon octicon-chevron-down HeaderMenu-icon ml-1'%3E%3Cpath fill-rule='evenodd' d='M12.78 6.22a.75.75 0 010 1.06l-4.25 4.25a.75.75 0 01-1.06 0L3.22 7.28a.75.75 0 011.06-1.06L8 9.94l3.72-3.72a.75.75 0 011.06 0z'%3E%3C/path%3E%3C/svg%3E"`;
 
 const Select = styled.select({
-  width: '49%',
+  width: '100%',
   border: '1px solid #f1f1f4',
   background: '#f1f2f7',
   padding: '8px 12px',
